49948fe7739c0e63b5656d935684c77c286a8104
rymuelle <rmueller@physics.tamu.edu>
2021-01-28 04:54:57 +0100

    working with basic histogram

--- mplStackPlot.py
+++ mplStackPlot.py
@@ -1,38 +1,23 @@
-from uproot_plotting_tools.basicHistStack import basicHistStack
-import numpy as np
-import matplotlib.pyplot as plt
-import mplhep as hep
-
 class mplStackPlot:
-    def __init__(self,name,bin_edges,y_list,y_list_var=[],y_list_sys=[],labels=[],colors=[]):
+    def __init__(self,name, bh1d_list=[],blinded=True):
         self.name = name
-        self.bin_edges = bin_edges
-        self.nBins = len(bin_edges)-1
-        self.y_list = np.asarray(y_list)
-        self.y_list_var = np.asarray(y_list_var)
-        self.y_list_sys = np.asarray(y_list_sys)
-        self.labels = list(labels)
-        self.colors = list(colors)
-        self.y_sum = np.sum(y_list,axis=0)
-        self.y_sum_var = np.sum(y_list_var,axis=0)
-        self.y_sum_list_sys = np.sum(y_list_sys,axis=0)
-    @classmethod
-    def from_basic_hist_stack_label(cls,hist_stack,label):
-        value_list, var_list, sys_list, plotting_kwargs_list = hist_stack.return_type(label)
-        colors = [x['color'] for x in plotting_kwargs_list ]
-        labels = [x['label'] for x in plotting_kwargs_list ]
-        args = [hist_stack.name, hist_stack.bin_edges, value_list]
-        kwargs = {"y_list_var":var_list, "y_list_sys":sys_list,"labels":labels,"colors":colors}
-        return cls(*args,**kwargs)        
-    @property
-    def y_list_std(self):
-        return self.y_list_var ** .5
-    @property
-    def bin_center(self):
-        return [(self.bin_edges[i]+self.bin_edges[i+1])/2. for i in range(self.nBins)]
-    @property
-    def y_list_std_sum(self):
-        return np.sum(self.y_list_var,axis=0) ** .5
+        self.n_hists = len(bh1d_list)
+        self.bin_edges = bh1d_list[0].bin_edges
+        self.n_bins = bh1d_list[0].n_bins
+        self.bh1d_list = bh1d_list
+        assert self.all_bins_match(), "All bins must match!"
+    def all_bins_match(self):
+        return np.asarray([self.bh1d_list[0].shares_bins( self.bh1d_list[i+1]) for i in range(self.n_hists-1)]).all()
+    def return_hist_stack(self,name,hist_mask,plot_kwargs={},category=None):
+        _temp_bh1d = basicHistogram1D("",self.bin_edges,plot_kwargs=plot_kwargs,category=category)
+        for mask, bh1d in zip(hist_mask,self.bh1d_list):
+            if mask: _temp_bh1d.add(bh1d, inherits=True)
+        return _temp_bh1d
+    def return_hist_by_label(self, label):
+        mask = [label==bh1d.label for bh1d in self.bh1d_list]
+        print(mask)
+        print( [bh1d.label for bh1d in self.bh1d_list])
+        return self.return_hist_stack("",mask)
     def histplot(self,kwargs,draw_sys=False):
         args = [
             self.y_list,
