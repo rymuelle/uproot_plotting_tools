e4fb03d9862a882ab34e8a40ff87cfb4cb0d02a7
rymuelle <rmueller@physics.tamu.edu>
2021-03-28 17:34:16 -0500

    updates to hist class and utils

--- Hist.py
+++ Hist.py
@@ -10,6 +10,8 @@ class Hist1D:
      
     '''
     def __init__(self, bin_edges, val, var, sys=Systematics(), name=0, title=0, category=0, plt_kwargs={}):
+        assert (len(bin_edges) == len(val)+1), "Wrong bin size"
+        assert (len(val)==len(var)), "Wrong var size" 
         self.bin_edges = deepcopy(bin_edges)
         self.val = np.array(deepcopy(val))
         self.var = np.array(deepcopy(var))
@@ -21,20 +23,33 @@ class Hist1D:
     @classmethod
     def from_uproot(cls, uproot_hist, kwargs={}):
         bin_edges = uproot_hist.to_numpy()[1]
-        bin_center = average_list_pairs(bin_edges)
         val = uproot_hist.to_numpy()[0]
         var = uproot_hist.variances()
         return cls(bin_edges,val,var,**kwargs)       
     def add(self,hist,kwargs={}):
         assert (list(self.bin_edges) == list(hist.bin_edges)), "must have same bins"
-        val = self.val, hist.val
+        val = self.val + hist.val
         var = self.var + hist.var
         sys = self.sys.add(hist.sys)
         name = "{} {}".format(self.name, hist.name)
         return Hist1D(self.bin_edges,val,var,sys=sys, **kwargs)
+    def stdev(self):
+        return self.var**.5
+    def bin_center(self):
+        return average_list_pairs(self.bin_edges)
     def __str__(self):
         return "Name: {}".format(self.name, np.sum(self.val), np.sum(self.var), self.bin_edges[0], self.bin_edges[-1])
     def __repr__(self):
-        return "Name: {} Sum:{} Var:{} Range:{} to {}".format(self.name, np.sum(self.val), np.sum(self.var), self.bin_edges[0], self.bin_edges[-1])
-    def standard_dev(self):
-        return self.var**.5
+        return "Name: {} Sum:{:.2f} Var:{:.2f} Range:{}-{} nBins: {}".format(self.name, np.sum(self.val), np.sum(self.var), self.bin_edges[0], self.bin_edges[-1],
+       self.n_bins())
+    def n_bins(self):
+        return len(self.val)
+    def range(self, min_range,max_range):
+        bins = self.bin_edges
+        mask = (bins>min_range)*(bins<max_range)
+        mask_reduced = np.asarray(average_list_pairs(mask*1))==1
+        return Hist1D(bins[mask], self.val[mask_reduced], self.var[mask_reduced],plt_kwargs=self.plt_kwargs)
+    def plot(self,ax,kwargs={}):
+        _kwargs = {**self.plt_kwargs,**kwargs}
+        ax.errorbar(self.bin_center(),self.val,yerr=self.stdev(),**_kwargs)
+    
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import uproot
 
+
 def get_hist_uproot(uproot_dir, hname, overflow=False):
     hist = uproot_dir[hname] 
     if overflow:
@@ -19,3 +20,55 @@ def safe_divide(a,b):
     return np.divide(a, b, out=np.zeros_like(a), where=b!=0)
 
 def average_list_pairs(l_list): return [(l_list[i] + l_list[i+1])/2 for i in range(len(l_list)-1)]
+
+
+def abcd_template(titles, yscale="linear",bottom=1):
+    import matplotlib
+    import matplotlib.pyplot as plt
+    import mplhep as hep
+    hep.set_style(hep.style.ROOT)
+
+    fig = plt.figure(constrained_layout=True, figsize=(10,16))
+    gs = fig.add_gridspec(10, 2)
+    ax = []
+    ax.append([fig.add_subplot(gs[0:3,0]), fig.add_subplot(gs[3,0])])
+    ax[0][0].set_title(titles[0])
+    ax[0][1].set_ylim(0,2)
+
+    ax.append([fig.add_subplot(gs[0:3,1],sharey=ax[0][0]), fig.add_subplot(gs[3,1])])
+    ax[1][0].set_title(titles[1])
+    ax[1][1].set_ylim(0,2)
+
+    ax.append([fig.add_subplot(gs[4:7,0],sharey=ax[0][0]), fig.add_subplot(gs[7,0])])
+    ax[2][0].set_title(titles[2])
+    ax[2][1].set_ylim(0,2)
+
+    ax.append([fig.add_subplot(gs[4:7,1],sharey=ax[0][0]), fig.add_subplot(gs[7,1])])
+    ax[3][0].set_title(titles[3])
+    ax[3][1].set_ylim(0,2)
+
+    for axis in ax:
+        axis[0].set_yscale(yscale)
+
+    return fig, ax
+
+def log_norm(x, norm, sigma, theta, mean,slope):
+    theta  = min(theta,140)
+    return norm/((x-theta)*sigma*2*3.14159)*np.exp(-(np.log((x-theta)/mean))**2/(2*sigma**2))*(slope*x)
+
+def log_norm_unc(x, norm, sigma, theta, mean,slope):
+    from uncertainties import unumpy
+    from uncertainties import ufloat
+    import uncertainties
+    theta  = min(theta,140)
+    return norm/((x-theta)*sigma*2*3.14159)*unumpy.exp(-(unumpy.log((x-theta)/mean))**2/(2*sigma**2))*(slope*x)
+
+
+    
+def make_band_plot(unc_array): 
+    from uncertainties import unumpy
+    from uncertainties import ufloat
+    import uncertainties
+    y = unumpy.nominal_values(unc_array)
+    y_err = unumpy.std_devs(unc_array)
+    return (y - y_err, y + y_err), y, y_err
